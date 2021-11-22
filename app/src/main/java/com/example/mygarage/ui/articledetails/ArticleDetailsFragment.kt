4428d7d0fae521ace8b35b4e7d124b572330835e
abeedev17 <as.asuperstar05@gmail.com>
2021-11-22 20:10:48 +0200

    ui changes to articles details screen

--- app/src/main/java/com/example/mygarage/ui/articledetails/ArticleDetailsFragment.kt
+++ app/src/main/java/com/example/mygarage/ui/articledetails/ArticleDetailsFragment.kt
@@ -1,16 +1,12 @@
 package com.example.mygarage.ui.articledetails
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mygarage.R
-import com.example.mygarage.ui.home.ArticleRecyclerViewAdapter
 import com.example.mygarage.ui.home.HomeViewModel
 import kotlinx.android.synthetic.main.fragment_article_details.*
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -19,9 +15,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class ArticleDetailsFragment : Fragment() {
 
     val args : ArticleDetailsFragmentArgs by navArgs()
-    companion object {
-        fun newInstance() = ArticleDetailsFragment()
-    }
+
 
     private val homeViewModel by viewModel<HomeViewModel>()
 
@@ -34,17 +28,12 @@ class ArticleDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val check = args.position
+        val itemPos = args.position
 
         homeViewModel.articleList.observe(viewLifecycleOwner, {
-            testTxt.text = it[check].title
+            titleTxt.text = it[itemPos].title
+            detailTxt.text = it[itemPos].article
         })
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
-    }
-
 
 }
