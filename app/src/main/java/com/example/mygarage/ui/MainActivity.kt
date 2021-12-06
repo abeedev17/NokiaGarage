cb2b5370bf425bb5c0534b3ac160e1379c5bceaf
abeedev17 <as.asuperstar05@gmail.com>
2021-12-06 17:12:09 +0200

    add splashscreen

--- app/src/main/java/com/example/mygarage/ui/MainActivity.kt
+++ app/src/main/java/com/example/mygarage/ui/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setTheme(R.style.Theme_MyGarage)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
